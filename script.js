a813fb6f3787d37ebe950763aa81e244fed31f5b
Giovanni Ponciano <ponciano.giovanni15@gmail.com>
2015-01-09 11:30:08 -0800

    pushing2

--- script.js
+++ script.js
@@ -7,6 +7,30 @@
 $('document').ready(function(){
     $(".gio").css("background-color", "green");
     
-    $("p").css("color", "white")
+    $("p").css("color", "white");
+    
+    $("phs").css("background-color", "red");
+    
+    $("p").css("color", "black");
+    
+    $('div:last p:nth-child(3)').css('background-color', 'red');
+     
+    $('p:last').css('color', 'red');
+    
+    $('p:gt(4)').css('border-style', 'solid');
+    
+    $('p:first').css({'background-color':'cyan', 'color':'black'});
+    
+    //$('div p:nth-child(1)').css('background-color', 'black');
+    
+    $("h1").bind('mouseover', mouseOverme).bind('mouseout', mouseOverme);
     
 });
+
+function mouseOverMe(){
+    $("h1").html("Computer Class");
+}
+
+function mouseOutMen(){
+    $('h1').html('Gio is awesome');
+}
